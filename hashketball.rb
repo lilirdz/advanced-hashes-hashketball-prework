c9d310010be92401de8b48d62ab739e246bc0161
Lili Rodriguez <lilimrod@gmail.com>
2020-03-31 04:47:32 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -187,7 +187,9 @@ def big_shoe_rebounds
   big_shoe = 0
   rebounds = 0
   game_hash.each do |team, team_info|
-    team_info[:players].each do |
+    team_info[:players].each do |player|
+      if [:players][:shoe] > big_shoe
+        return 
   end
 end
 
