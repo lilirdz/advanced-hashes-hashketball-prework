8d604dd9a7947ce80d6659b6da6d0936568f0c42
Lili Rodriguez <lilimrod@gmail.com>
2020-03-31 04:32:06 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -153,7 +153,7 @@ def team_names
 end
 
 def player_numbers(team_name)
-  jersey_numbers = {}
+  jersey_numbers = []
   game_hash.each do |team, team_info|
     if team_info[:team_name] == team_name
       team_info.each do |key, value|
